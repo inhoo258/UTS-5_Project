be28c924806494d3cce4f7c739ae220d5acfef96
seungwoo <seungwoo@DESKTOP-CMPGJ7J>
2020-07-12 16:55:58 +0900

    0712 16:55 product/cart - 개인 장바구니 조회 페이지 생성 및 컨트롤러 연결

--- src/main/java/com/spring/project/product/model/CartVO.java
+++ src/main/java/com/spring/project/product/model/CartVO.java
@@ -4,6 +4,52 @@ public class CartVO {
 	private String member_id;
 	private int product_id;
 	private int cart_product_count;
+	private String product_info;
+	private String product_name;
+	private String product_price;
+	private byte[] product_img;
+	private String seller_name;
+	
+
+	public String getSeller_name() {
+		return seller_name;
+	}
+
+	public void setSeller_name(String seller_name) {
+		this.seller_name = seller_name;
+	}
+
+	public String getProduct_info() {
+		return product_info;
+	}
+
+	public void setProduct_info(String product_info) {
+		this.product_info = product_info;
+	}
+
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	public String getProduct_price() {
+		return product_price;
+	}
+
+	public void setProduct_price(String product_price) {
+		this.product_price = product_price;
+	}
+
+	public byte[] getProduct_img() {
+		return product_img;
+	}
+
+	public void setProduct_img(byte[] product_img) {
+		this.product_img = product_img;
+	}
 
 	public String getMember_id() {
 		return member_id;
--- src/main/webapp/WEB-INF/views/product/cart.jsp
+++ src/main/webapp/WEB-INF/views/product/cart.jsp
@@ -1,12 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>cart</title>
 </head>
 <body>
-
+<h1>cart page</h1>
+${cartList}
+<c:forEach var="cart" items="${cartList}">
+	${cart.member_id}<br>
+	${cart.product_id}<br>
+	${cart.cart_product_count}<br>
+	${cart.product_info}
+	${cart.product_name}
+	${cart.product_price}
+	${cart.product_img}
+</c:forEach>
 </body>
 </html>
