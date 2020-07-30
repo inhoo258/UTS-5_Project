18fb6cf3214e5035512c0d735ad2d5a1ba6432ba
jihyeon <jihyeon@DESKTOP-PKMHK1B>
2020-07-30 21:41:36 +0900

    .

--- src/main/java/com/spring/project/board/controller/BoardController.java
+++ src/main/java/com/spring/project/board/controller/BoardController.java
@@ -160,3 +160,165 @@ public class BoardController {
 	}
 	
 }
+package com.spring.project.board.controller;
+
+import java.io.IOException;
+import java.lang.reflect.Member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import com.spring.project.board.service.EventService;
+import com.spring.project.board.model.NoticeVO;
+import com.spring.project.board.service.NoticeService;
+import com.spring.project.board.service.QnAService;
+import com.spring.project.board.service.ReviewService;
+import com.spring.project.common.PagingManager;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	@Autowired
+	QnAService qnAService;
+	@Autowired
+	ReviewService reviewService;
+	@Autowired
+	EventService eventService;
+	@Autowired
+	NoticeService noticeService;
+	
+	//공지사항 게시판
+	@GetMapping("/notice/list")
+	public void noticeList(@RequestParam(value = "page", required = false, defaultValue = "1") int page, Model model) {
+		System.out.println("requested page : "+page);
+		model.addAttribute("noticeList", noticeService.getNoticeList(page));
+		PagingManager pagingManager = new PagingManager(noticeService.getTotalCount(), page);
+		System.out.println("now page : " + pagingManager.getNowPage());
+		System.out.println("end page : " + pagingManager.getEndPage());
+		System.out.println("now block : " + pagingManager.getNowBlock());
+		System.out.println("total page : " + pagingManager.getTotalPage());
+		System.out.println("total block : " + pagingManager.getTotalBlock());
+		model.addAttribute("pagingManager", pagingManager);
+	}
+
+	@GetMapping("/notice/{notice_rn}")
+	public String noticeView(Model model, @PathVariable("notice_rn") int notice_rn) {
+		model.addAttribute("notice", noticeService.getNotice(notice_rn));
+		int totalCount = noticeService.getTotalCount();
+		model.addAttribute("totalCount",totalCount);
+		if(notice_rn!=1)model.addAttribute("preTitle",noticeService.getTitle(notice_rn-1));
+		if(notice_rn!=totalCount)model.addAttribute("postTitle",noticeService.getTitle(notice_rn+1));
+//		System.out.println("totalCount : "+totalCount);
+//		System.out.println("notice_rn : "+notice_rn);
+		return "board/notice/view";
+	}
+
+	@GetMapping("/notice/form")
+	public String noticeForm(Model model) {
+		model.addAttribute("msg", "new");
+		return "board/notice/form";
+	}
+	
+	//notice view에서 수정 버튼 클릭 시	
+	@GetMapping("/notice/form/{notice_rn}")
+	public String getView(@PathVariable("notice_rn") int notice_rn, Model model) {
+		model.addAttribute("notice", noticeService.getNoticeInfo(notice_rn) );
+		model.addAttribute("msg" , "update");
+		return "board/notice/form";
+	}
+
+	//notice view 수정
+	@PostMapping("/notice/update")
+	public String updateView(NoticeVO noticeVo ) {
+		noticeService.updateView(noticeVo);
+		return "redirect:/board/notice/"+noticeVo.getNotice_rn();
+	}
+	
+	
+	@PostMapping("/notice/new")
+	public String noticeInsert(@ModelAttribute NoticeVO noticeVO,
+			@RequestParam(value = "file", required = false) MultipartFile file) {
+		if (file != null&&!file.isEmpty()) {
+			try {
+				noticeVO.setNotice_file(file.getBytes());
+				System.out.println("getBytes : "+file.getBytes());
+				noticeVO.setNotice_file_name(file.getOriginalFilename());
+				System.out.println("originalFileName : "+file.getOriginalFilename());
+			} catch (IOException e) {
+			}
+			noticeService.insertNoticeWithFile(noticeVO);
+		}
+		else noticeService.insertNotice(noticeVO);
+		return "redirect:/board/notice/list";
+	}
+	
+	// notice view 삭제 
+	@GetMapping("/notice/delete/{notice_number}")
+	public String deleteView(@PathVariable("notice_number") int notice_number) {
+		noticeService.deleteView(notice_number);
+		return "redirect:/board/notice/list";
+	}
+	
+	//이벤트 게시판
+	@GetMapping("/event/list")
+	public void eventList(@RequestParam(value="page", required = false, defaultValue = "1")int page, Model model) {
+		model.addAttribute("eventList",eventService.getEventList(page));
+		PagingManager pagingManager = new PagingManager(eventService.getTotalCount(), page);
+		model.addAttribute("pagingManager",pagingManager);
+	}
+	
+	@GetMapping("/event/{event_rn}")
+	public String eventView(Model model, @PathVariable("event_rn")String event_rn) {
+		int totalCount = eventService.getTotalCount();
+		model.addAttribute("event",eventService.getEvent(event_rn));
+		model.addAttribute("totalCount",totalCount);
+		return "board/event/view";
+	}
+	
+	@GetMapping("/event/form")
+	public void eventForm() {
+		
+	}
+	
+	//qna 게시판
+	@GetMapping("/qna/list")
+	public void qnaList(@RequestParam(value="page", required = false, defaultValue = "1")int page, Model model) {
+		model.addAttribute("eventList",eventService.getEventList(page));
+		PagingManager pagingManager = new PagingManager(eventService.getTotalCount(), page);
+		model.addAttribute("pagingManager",pagingManager);
+	}
+	
+	@GetMapping("/qna/form")
+	public void qnaForm() {
+		
+	}
+	
+	//리뷰 게시판
+	@GetMapping("/review/list")
+	public void reviewList(@RequestParam(value="page", required = false, defaultValue = "1")int page, Model model) {
+		model.addAttribute("eventList",eventService.getEventList(page));
+		PagingManager pagingManager = new PagingManager(eventService.getTotalCount(), page);
+		model.addAttribute("pagingManager",pagingManager);
+	}
+	
+	@GetMapping("/review/{member_id}")
+	public String reviewView(Model model, @PathVariable("member_id")String member_id) {
+		int totalCount = reviewService.getTotalCount();
+		model.addAttribute("review",reviewService.getReview(member_id));
+		model.addAttribute("totalCount",totalCount);
+		return "board/event/view";
+	}
+	
+	@GetMapping("/review/form")
+	public void reviewForm() {
+		
+	}
+	
+}
