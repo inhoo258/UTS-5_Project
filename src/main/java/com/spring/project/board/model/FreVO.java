57563c2d12abfcb75c242d49bc50e8c8313089de
choesj <choesj@Naver.com>
2020-08-12 16:53:58 +0900

    freVO 생성

---
+++ src/main/java/com/spring/project/board/model/FreVO.java
@@ -0,0 +1,26 @@
+package com.spring.project.board.model;
+
+public class FreVO {
+	private String fre_title;
+	private String fre_cate;
+	private String fre_content;
+
+	public String getFre_title() {
+		return fre_title;
+	}
+	public void setFre_title(String fre_title) {
+		this.fre_title = fre_title;
+	}
+	public String getFre_Cate() {
+		return fre_cate;
+	}
+	public void setFre_Cate(String cate) {
+		this.fre_cate = cate;
+	}
+	public String getFre_content() {
+		return fre_content;
+	}
+	public void setFre_content(String fre_content) {
+		this.fre_content = fre_content;
+	}
+}
